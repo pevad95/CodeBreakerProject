1b18fa8853213ccc9aec39affec3026c5f5a7614
Vadasz Peter <peter.vadasz@p92rdi.hu>
2017-08-11 16:18:15 +0200

    Call setHiddenFields.

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,12 +4,16 @@ let attempt = document.getElementById('attempt');
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
+    if (answer.value == '' && attempt.value == '') {
+        setHiddenFields();
+    }
 }
 
 //implement new functions here
 
 function setHiddenFields() {
     answer.value = Math.floor(Math.random() * 10000).toString();
+    attempt.value = 0;
     while (answer.value.length < 4) {
         answer.value = "0" + answer.value;
     }
