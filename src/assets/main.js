78ca986a9d1c88e4410bab666aeabe39c5d07d8c
Vadasz Peter <peter.vadasz@p92rdi.hu>
2017-08-14 10:24:21 +0200

    Fix showReplay function.

--- src/assets/main.js
+++ src/assets/main.js
@@ -73,6 +73,6 @@ function showAnswer(won) {
 }
 
 function showReplay() {
-    document.getElementById('guessing-div').style.display = none;
-    document.getElementById('replay-div').style.display = block;
+    document.getElementById('guessing-div').style.display = 'none';
+    document.getElementById('replay-div').style.display = 'block';
 }
